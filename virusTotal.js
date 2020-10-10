83b212c4621a890e38ff7438ee9bb622b7910845
galbar07 <53149608+galbar07@users.noreply.github.com>
2020-10-10 12:58:38 +0300

    Adding engines detection to the CSV

--- virusTotal.js
+++ virusTotal.js
@@ -15,12 +15,27 @@ const theSameObject = defaultTimedInstance.ipLookup(line, function(err, res){
     return;
   }
   const obj = JSON.parse(res);
+  let engines = "";
+  
+  if(obj.data.attributes.last_analysis_stats.malicious>0){
+    let scan = obj.data.attributes.last_analysis_results;
+    console.log(scan);
+    Object.entries(scan).forEach(
+      ([key, value]) => {
+        if(value.result === "malicious" || value.result === "malware"){
+        engines += key + "\n";
+       }
+       
+      }
+  );
+  }
   someData.push({ "Ip" : line,
   "harmless":obj.data.attributes.last_analysis_stats.harmless,
   "malicious":obj.data.attributes.last_analysis_stats.malicious,
   "suspicious":obj.data.attributes.last_analysis_stats.suspicious,
     "country" : obj.data.attributes.country ,
-    "owner":obj.data.attributes.as_owner});
+    "owner":obj.data.attributes.as_owner,
+  "Engine detection" : engines});
  
   converter.json2csv(someData, (err, csv) => {
     if (err) {
